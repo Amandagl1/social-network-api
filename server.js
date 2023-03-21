089ba391e727df85c48cd9ba2ea79565c8dc4102
Amanda Lee <leeandama87@gmail.com>
2023-03-21 13:52:31 -0700

    Edited server file.

--- server.js
+++ server.js
@@ -6,14 +6,13 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
 app.use(require("./routes"));
 
 
 //--Setting up MongoDB connection--
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/social-Trex",
+    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/social-network-api",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -22,4 +21,6 @@ mongoose.connect(
   
   mongoose.set("debug", true);
   
-  app.listen(PORT, () => console.log(`App connected! Listening on port:${PORT}!`));
+  
+  app.listen(PORT, () => 
+  console.log(`Connected to server! Listening on port:${PORT}!`));
